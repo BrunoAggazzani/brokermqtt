f79e2dcb508722e5bd8334f89d9e03211478ef75
Bruno Aggazzani <baggazzani@gmail.com>
2020-12-02 19:40:48 -0300

    procfile prueba

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.listen(app.get('port'), () => {
     var mosca = require('mosca');
     var setting = {port: 443};
     var broker = new mosca.Server(setting);
-
+    
     broker.on('ready', () => {
         console.log('broker listo!');
     });
